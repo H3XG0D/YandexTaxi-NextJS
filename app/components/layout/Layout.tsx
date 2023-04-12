d81e3d6f27a90732609bb8bc972c1469e159fb5b
H3XG0D <99123233+H3XG0D@users.noreply.github.com>
2023-04-13 08:51:16 +0900

    Added loader when app is starting

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,4 +1,8 @@
-import { FC, useState } from "react";
+import Head from "next/head";
+import taxiIcon from "../../assets/images/taxiIcon.png";
+import { FC, useEffect, useState } from "react";
+import Script from "next/script";
+import Loader from "../UI/Loader";
 
 interface ILayout {
   title: string;
@@ -8,7 +12,39 @@ interface ILayout {
 const Layout: React.FC<ILayout> = ({ children, title }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  return <div>{children}</div>;
+  useEffect(() => {
+    setIsLoading(true);
+
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>{title} | React Taxi</title>
+        <meta name="description" content="Created by Sivtsev Ivan"></meta>
+        <link rel="shortcut icon" href={taxiIcon.src} type="image/png"></link>
+      </Head>
+
+      <Script
+        strategy="beforeInteractive"
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.MAP_KEY}&libraries=places`}
+      />
+
+      <div
+        style={{ maxWidth: 480 }}
+        className="mx-auto relative overflow-hidden"
+      >
+        {isLoading ? <Loader /> : children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
